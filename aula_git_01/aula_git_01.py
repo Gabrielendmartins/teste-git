d358f1bd11407930b134e30cbcf688ee4960d19f
GABRIEL HENRIQUE MARTINS DA SILVA ROSA <gabriel.rosa50@senaisp.edu.br>
2026-04-22 11:08:48 -0300

    Ola meu nome e Gabrielhenrique e tenho 16 anos de idade

--- aula_git_01/aula_git_01.py
+++ aula_git_01/aula_git_01.py
@@ -1,6 +1,6 @@
-nome = input("Gabriel")
+nome = input("Gabriel Henrique")
 
 idade = int(input("Digite a sua idade"))
 
-print(F"Ola meu nome e {Gabriel} e tenho {16} anos de idade")
+print(F"Ola meu nome e {Gabrielhenrique} e tenho {16} anos de idade")
 
